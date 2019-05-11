c3beaa718c20e9c9ea45903f80b7c6fe36fc578a
Koray Dundar <koraydundar_40@hotmail.com>
2019-05-11 16:56:13 +0300

    added react-router-dom and imported Browser router as Router, Switch, Route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import './App.css'
+
 import Nav from './Nav'
 import About from './pages/About'
 import Shop from './pages/Shop'
 import Contact from './pages/Contact'
 
-import './App.css'
 
 function App() {
   return (
