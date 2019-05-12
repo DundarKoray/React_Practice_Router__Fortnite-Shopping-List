aba491128d3cce829893303268d253e6201d562b
Koray Dundar <koraydundar_40@hotmail.com>
2019-05-12 15:06:06 +0300

    practice is completed

--- src/pages/ItemDetail.js
+++ src/pages/ItemDetail.js
@@ -3,21 +3,33 @@ import '../App.css'
 
 
 
-const Item = () => {
+const ItemDetail = ({ match }) => {
 
     useEffect(()=>{
-        
-    },[])
+        fetchItem()
+        console.log(match)
+    }, [])
 
-    const [item, setItem] = useState({})
+    const [item, setItem] = useState({
+        images: {}
+    })
 
+    const fetchItem = async () => {
+        const fetchItem = await fetch (`https://fortnite-public-api.theapinetwork.com/prod09/item/get?ids=${match.params.id}`)
+
+
+        const item = await fetchItem.json()
+        setItem(item)
+        console.log(item)
+    }
     
 
     return(
         <div>
-           <h1>Item</h1>
+           <h1>{item.name}</h1>
+           <img src={item.images.transparent} alt="" />
         </div>
     )
 }
 
-export default Item
+export default ItemDetail
