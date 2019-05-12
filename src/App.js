ebdf6465cac031fa00d7b4dc53e77365e9c8df08
Koray Dundar <koraydundar_40@hotmail.com>
2019-05-12 14:12:05 +0300

    fetching data from Fortnite

--- src/App.js
+++ src/App.js
@@ -10,14 +10,27 @@ import Contact from './pages/Contact'
 
 function App() {
   return (
-    <div>
-      <Nav />
-      <About />
-      <Shop />
-      <Contact />
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About}/>
+          <Route path="/shop" component={Shop}/>
+          <Route path="/contact" component={Contact}/>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  )
+}
 
 export default App;
--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 const Nav = () => {
+    const navStyle = {
+        color: 'white'
+    }
+
+
     return (
         <nav>
             <h3>Logo</h3>
             <ul className="nav-links">
-                <li>About</li>
-                <li>Shop</li>
-                <li>Contact</li>
+                <Link to="/about" style={navStyle}><li>About</li></Link>
+                <Link to="/shop" style={navStyle}><li>Shop</li></Link>    
+                <Link to="/contact" style={navStyle}><li>Contact</li></Link>
             </ul>
         </nav>
     )
